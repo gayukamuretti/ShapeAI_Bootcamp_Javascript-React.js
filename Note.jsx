07790ef163e6dd68b92193feedb1562dd031f200
Gayathri Kamuretti <58182530+gayukamuretti@users.noreply.github.com>
2021-06-06 10:55:55 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        Web dev bootcamp was an amazing taken up by Shaurya Sinha. He covered so
+        many topics which is cleared and understand.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
